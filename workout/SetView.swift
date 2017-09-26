7a92d5592240eb18e6148df0ea2f5d525de5e8e8
Joseph <Ethan.Joseph@tdameritrade.com>
2017-09-25 23:38:40 -0400

    toggle working

--- workout/SetView.swift
+++ workout/SetView.swift
@@ -3,5 +3,22 @@ import UIKit
 class SetView: UIView {
     
     @IBOutlet weak var hexagon: UIImageView!
+    @IBOutlet weak var toggle: UISwitch!
     
+    var isSetComplete: Bool = false
+    
+    func setComplete(){
+        self.hexagon.image = UIImage.init(named: "hexagon_border")
+        isSetComplete = true
+    }
+    
+    func setIncomplete(){
+        self.hexagon.image = UIImage.init(named: "hex_gray_border")
+        isSetComplete = false
+    }
+    
+    @IBAction func toggleComplete(_ sender: Any) {
+        self.hexagon.image = isSetComplete ? UIImage.init(named: "hex_gray_border") : UIImage.init(named: "hexagon_border")
+        isSetComplete = !isSetComplete
+    }
 }
